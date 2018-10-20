898468a529df126c61b73e01f52fbb760702acbd
mao420430 <645267152@qq.com>
2018-10-20 19:05:18 +0800

    http post/get 服务器端

--- xiaoserver/src/main/java/com/xiao/server/controller/LoginLog.java
+++ xiaoserver/src/main/java/com/xiao/server/controller/LoginLog.java
@@ -1,13 +1,24 @@
 package com.xiao.server.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class LoginLog {
-    @RequestMapping("/login")
-    public String Login(){
-        return "Login  hot";
+    //@ResponseBody
+    @RequestMapping(value="/login", method = RequestMethod.GET)
+    public String Login(@RequestParam("name") String name, @RequestParam("pwd") String pwd){
+        System.out.println("-----------login GET");
+        System.out.println("name:"+name);
+        System.out.println("pwd: "+pwd);
+        return ("login:"+name+"  "+pwd);
+    }
+
+    @RequestMapping(value="/login", method = RequestMethod.POST)
+    public String Login2(@RequestParam("name") String name, @RequestParam("pwd") String pwd){
+        System.out.println("-----------login POST");
+        System.out.println("name:"+name);
+        System.out.println("pwd: "+pwd);
+        return ("login:"+name+"  "+pwd);
     }
 
 
